16cdc3658671d93ff246c855a5efc8a4ad87fb73
Gan Hao Yi <ganhaoyi.100@gmail.com>
2024-09-07 23:35:04 +0800

    Resolve merge conflicts

--- src/main/java/com/product/service/category/CategoryService.java
+++ src/main/java/com/product/service/category/CategoryService.java
@@ -1,5 +1,7 @@
 package com.product.service.category;
 
+import com.product.dto.ResponseMessageDTO;
+import com.product.dto.category.*;
 import com.product.entity.Category;
 import com.product.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +13,32 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class CategoryService {
-
+    private final CategoryCreationService categoryCreationService;
+    private final CategoryReadService categoryReadService;
+    private final CategoryUpdateService categoryUpdateService;
+    private final CategorySearchService categorySearchService;
+    private final CategoryDeleteService categoryDeleteService;
     private final CategoryRepository categoryRepository;
+
+    public ResponseMessageDTO handleCreateCategory(String messageId, CategoryCreationRequestDTO categoryCreationRequestDTO) {
+        return categoryCreationService.createCategory(messageId, categoryCreationRequestDTO);
+    }
+
+    public ResponseMessageDTO handleReadCategory(String messageId, CategoryReadRequestDTO categoryReadRequestDTO) {
+        return categoryReadService.readCategory(messageId, categoryReadRequestDTO);
+    }
+
+    public ResponseMessageDTO handleUpdateCategory(String messageId, CategoryUpdateRequestDTO categoryUpdateRequestDTO) {
+        return categoryUpdateService.updateCategory(messageId, categoryUpdateRequestDTO);
+    }
+
+    public ResponseMessageDTO handleDeleteCategory(String messageId, CategoryDeleteRequestDTO categoryDeleteRequestDTO) {
+        return categoryDeleteService.deleteCategory(messageId, categoryDeleteRequestDTO);
+    }
+
+    public ResponseMessageDTO handleSearchCategory(String messageId, CategorySearchRequestDTO categorySearchRequestDTO) {
+        return categorySearchService.searchCategory(messageId, categorySearchRequestDTO);
+    }
 
     public Category saveCategoryIfNotExists(String categoryName) {
         return categoryRepository.findByName(categoryName).orElseGet(() -> categoryRepository.save(new Category(categoryName)));
