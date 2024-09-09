98609dfd55786feb2474c0417ef1f24d39b2a348
Yeek Sheng <104289235+Yeek020407@users.noreply.github.com>
2024-09-09 14:16:57 +0800

    Update config file

--- src/main/java/com/product/Application.java
+++ src/main/java/com/product/Application.java
@@ -4,11 +4,13 @@ import com.product.config.PubSubConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigurationProperties(PubSubConfiguration.class)
-@RequestMapping
+@RestController
 public class Application {
 
     public static void main(String[] args) {
@@ -21,3 +23,4 @@ public class Application {
     }
 
 }
+
